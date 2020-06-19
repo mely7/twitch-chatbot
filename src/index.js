79c9334ec9f3a76e0d12331eeb097d92af068f61
Melissa Garza <melissa.garza@utexas.edu>
2020-06-19 14:28:14 -0500

    Change action based on type of message

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const tmi = require('tmi.js');
 require('dotenv').config();
 
 const client = new tmi.Client({
-  options: { debug: true },
+  options: {
+    debug: true
+  },
   connection: {
     reconnect: true,
     secure: true
@@ -12,15 +14,24 @@ const client = new tmi.Client({
     username: process.env.TWITCH_BOT_NAME,
     password: process.env.TWITCH_TOKEN
   },
-  channels: [ process.env.TWITCH_CHANNEL ]
+  channels: [process.env.TWITCH_CHANNEL]
 });
 
 client.connect();
 
-client.on('message', (channel, tags, message, self) => {
-  if(self) return;
+client.on("message", (channel, userstate, message, self) => {
+  if (self) return;
 
-  if(message.toLowerCase() === '!hello') {
-    client.say(channel, `@${tags.username}, heya!`);
+  console.log(userstate);
+
+  switch (userstate["message-type"]) {
+    case "action":
+      break;
+    case "chat":
+      break;
+    case "whisper":
+      break;
+    default:
+      break;
   }
 });
